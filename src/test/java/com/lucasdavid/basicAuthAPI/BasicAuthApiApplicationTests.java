b9bd926177aa12d928fd514a2c16b98b28fc97ed
Lucas David <santoslucasdavid@gmail.com>
2025-02-22 20:49:48 -0300

    Project created

---
+++ src/test/java/com/lucasdavid/basicAuthAPI/BasicAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdavid.basicAuthAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
