446df18025dd758aab948de1c1e378464a36ed3a
Lucas David <santoslucasdavid@gmail.com>
2025-03-03 19:21:10 -0300

    User logic updated

--- src/main/java/com/lucasdavid/basicAuthAPI/resources/UserResource.java
+++ src/main/java/com/lucasdavid/basicAuthAPI/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.lucasdavid.basicAuthAPI.entities.User;
 import com.lucasdavid.basicAuthAPI.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -21,5 +20,11 @@ public class UserResource {
     public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = service.insert(obj);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
 }
