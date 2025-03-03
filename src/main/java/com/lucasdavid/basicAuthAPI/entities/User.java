f80a39b9969d4b123543e5a9d994562b1d5ae142
Lucas David <santoslucasdavid@gmail.com>
2025-03-02 21:23:11 -0300

    User entity and application properties updated and database properties file created

--- src/main/java/com/lucasdavid/basicAuthAPI/entities/User.java
+++ src/main/java/com/lucasdavid/basicAuthAPI/entities/User.java
@@ -1,10 +1,16 @@
 package com.lucasdavid.basicAuthAPI.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_users")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
